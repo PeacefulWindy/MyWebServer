3726e8c348d780f007c881216c66be0ecfdf1afe
PeacefulWindy <a1056419426dgmail.com>
2020-02-29 15:37:56 +0800

    修改bug 修改为127.0.0.1

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-
 const express = require("express");
 const app = express();
 const port = 8000;//设置端口号，如果端口号被占用需要自己修改，否则无法跑起来(建议不要用80和8080，一般情况都会被占用)
-app.listen(port, () => console.log(`http://127.0.0.1:${port}`));//打印一下接口用例地址
+app.listen(port,"127.0.0.1",() => console.log(`http://127.0.0.1:${port}`));//打印一下接口用例地址
 app.use(require("body-parser").json());
 app.use(require("body-parser").urlencoded({extended: false}));
 //设置跨域访问
