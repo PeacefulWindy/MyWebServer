535022cb819dc6f7008ccd9e73f3176e59adc78b
PeacefulWindy <a1056419426@gmail.com>
2020-11-04 15:51:44 +0800

    更新手机价格功能

--- phonePrice/phonePrice.js
+++ phonePrice/phonePrice.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const request=require("request");
 const cheerio = require('cheerio');
 module.exports = router;
-const config = require("../config/mysql");
+const config=require("../config/config")
+const mysqlCfg = require("../config/mysql");
 const mysql = require("mysql");
-const conn = mysql.createConnection(config);
+const conn = mysql.createConnection(mysqlCfg);
 const iconv = require("iconv-lite");
 
 class PhoneInfo
@@ -22,12 +23,12 @@ class PhoneInfo
     RAM="";
     ROM="";
     markTime="";
+    mainScreenType="";
 }
 
 router.get("/submit", (req, res) => 
 {
     let phoneName=req.query.phoneName
-    console.log(req.query)
     if(!phoneName)
     {
         return res.json(
@@ -37,49 +38,22 @@ router.get("/submit", (req, res) =>
         });
     }
 
-    getPhoneName(phoneName,function(phoneNames)
+    let url="https://model.super202.cn/getJixingData.html?page=1&limit=15&sousuo="+phoneName;
+    httpRequest(url,function(content)
     {
-        if(phoneNames.length!=0)
+        let js=JSON.parse(content);
+        if(js.data.length==0)
         {
-            return res.json(
-            {
-                code: 200,
-            });
+            return;
         }
 
-        let url="https://model.super202.cn/getJixingData.html?page=1&limit=15&sousuo="+phoneName;
-        httpRequest(url,function(content)
-        {
-            let js=JSON.parse(content);
-            if(js.data.length==0)
-            {
-                return;
-            }
+        let data=js.data[0];
+        searchInfo(data.name);
+    },null);
 
-            let data=js.data[0];
-            let keys=["name","brand","models"];
-            let values=[data.name,data.pinpai,data.jixing];
-
-            insertSql("phoneprice_phonename",keys,values,function()
-            {
-                return res.json(
-                {
-                    code: 200,
-                });
-            },function(err)
-            {
-                return res.json(
-                {
-                    code: 200,
-                });
-            });
-        },function(err)
-        {
-            return res.json(
-            {
-                code: 200,
-            });
-        },null);
+    return res.json(
+    {
+        code: 200,
     });
 });
 
@@ -94,191 +68,105 @@ router.get("/update",function(req,res)
 
         phoneNames.forEach(phoneName => 
         {
-            let cookie="Hm_lpvt_ae5edc2bc4fc71370807f6187f0a2dd0="+Date.parse(new Date());
-            let url="http://detail.zol.com.cn/index.php?c=SearchList&subcateId=57&keyword="+phoneName.name;
-            url=encodeURI(url);
+            let randTime=Math.random()*phoneNames.length*10000+2000;
+            setTimeout(() => {
+                searchInfo(phoneName.name);
+            },randTime);
+        });
+    });
 
-            let randTime=Math.random()*7000+3000;
-            
+    return res.json
+    ({
+        "code":200,
+    });
+});
+
+router.get("/updateFromName",function(req,res)
+{
+    let phoneName=req.query.name;
+    if(!phoneName)
+    {
+        return res.json
+        ({
+            "code":403,
+            "err":"invaild request",
+        });
+    }
+
+    searchInfo(phoneName);
+
+    return res.json(
+    {
+        code:200
+    });
+});
+
+router.get("/search",function(req,res)
+{
+    let pinpaiID=0;
+    if(req.query.pinpai)
+    {
+        pinpaiID=req.query.pinpai;
+    }
+
+    let money="1";
+    if(req.query.minMoney)
+    {
+        if(req.query.maxMoney)
+        {
+            money=req.query.minMoney+"-"+req.query.maxMoney;
+        }
+        else
+        {
+            money=req.query.minMoney;
+        }
+    }
+
+    let page=1;
+    let url="http://detail.zol.com.cn/cell_phone_index/subcate57_"+pinpaiID+"_list_"+money+"_0_1_1_0_"+page+".html";
+    httpRequest(url,function(content)
+    {
+        content=iconv.decode(content,"gb2312");
+        let $=cheerio.load(content);
+        let pageHtml=$("span[class='small-page-active']").text();
+        let maxPage=0;
+        if(pageHtml!="")
+        {
+            let index=pageHtml.indexOf("/");
+            if(index != -1)
+            {
+                maxPage=pageHtml.substr(index+1,pageHtml.length-index);
+            }
+        }
+
+        for(let page=1;page<=maxPage;page++)
+        {
+            let randTime=Math.random()*maxPage*10000+2000;
             setTimeout(function()
             {
+                url="http://detail.zol.com.cn/cell_phone_index/subcate57_"+pinpaiID+"_list_"+money+"_0_1_1_0_"+page+".html";
                 httpRequest(url,function(content)
                 {
                     content=iconv.decode(content,"gb2312");
                     let $=cheerio.load(content);
-                    let mods=$("div[class=pro-intro]");
-                    let prices=$("div[class='price-box']");
-
-                    for(let i=0;i<mods.length;i++)
+                    let nodes=$("div[class='pro-intro'] h3 a");
+                    for(let i=0;i<nodes.length;i++)
                     {
-                        let it=mods[i];
-                        let priceNode=prices[i];
-
-                        let phone=new PhoneInfo();
-
-                        phone.name=$("a[class=title]",it).text();
-                        phone.price=$("b[class='price-type']",priceNode).text();
-
-                        let ul=$("ul[class='param clearfix']",it);
-
-                        let as=$("a",ul);
-                        for(let i=0;i<as.length;i++)
+                        let randTime=Math.random()*nodes.length*10000+2000;
+                        setTimeout(function()
                         {
-                            let a=as[i];
-                            if(a.children[0].data=="更多参数>>")
-                            {
-                                phone.link="http://detail.zol.com.cn"+a.attribs.href;
-                                break;
-                            }
-                        }
-                        
-                        if(phone.link=="")
-                        {
-                            continue;
-                        }
-
-                        getPhoneInfoID(phone.name,function(id)
-                        {
-                            let randTime=Math.random()*7000+3000;
-                            setTimeout(function(args)
-                            {
-                                httpRequest(phone.link,function(content,args)
-                                {
-                                    let id=args.id;
-                                    let phone=args.phone;
-                                    content=iconv.decode(content,"gb2312");
-                                    let $=cheerio.load(content);
-                                    let detailedParameters=$("div[class='detailed-parameters']");
-                                    let ths=$("th",detailedParameters);
-                                    let tds=$("td",detailedParameters);
-                                    for(let i=0;i<ths.length;i++)
-                                    {
-                                        let title=$("#newPmName_"+i,ths).text();
-                                        let value=$("#newPmVal_"+i,tds).text();
-
-                                        switch(title)
-                                        {
-                                        case "出厂系统内核":
-                                            phone.system=value;
-                                            break;
-                                        case "CPU型号":
-                                            phone.CPU=value;
-                                            break;
-                                        case "主屏分辨率":
-                                            phone.mainScreen=value;
-                                            break;
-                                        case "CPU频率":
-                                            phone.CPUrate=value;
-                                            break;
-                                        case "GPU型号":
-                                            phone.GPU=value;
-                                            break;
-                                        case "RAM容量":
-                                            phone.RAM=value;
-                                            break;
-                                        case "ROM容量":
-                                            phone.ROM=value;
-                                            break;
-                                        case "电池容量":
-                                            phone.power=value;
-                                            break;
-                                        case "上市日期":
-                                            phone.markTime=value;
-                                            break;
-                                        }
-                                    }
-
-                                    let sqlKeys=
-                                    [
-                                        "name","mainScreen","CPU",
-                                        "CPUrate","system","power",
-                                        "price","link","GPU",
-                                        "RAM","ROM","markTime",
-                                        "updateTime"
-                                    ];
-
-                                    let sqlValue=
-                                    [
-                                        phone.name,phone.mainScreen,phone.CPU,
-                                        phone.CPUrate,phone.system,phone.power,
-                                        phone.price,phone.link,phone.GPU,
-                                        phone.RAM,phone.ROM,phone.markTime,
-                                        "now()"
-                                    ];
-
-                                    if(id==0)
-                                    {
-                                        insertSql("phoneprice_phoneinfo",sqlKeys,sqlValue);
-                                    }
-                                    else
-                                    {
-                                        updateSql("phoneprice_phoneinfo",id,sqlKeys,sqlValue);
-                                    }
-                                    
-                                },function(err)
-                                {
-                                    return res.json(
-                                    {
-                                        code: 403,
-                                        data: err,
-                                    });
-                                },null,{"phone":phone,"id":args.id});
-                            },randTime,{"id":id});
-                        },
-                        function(err)
-                        {
-                            
-                        });
+                            searchInfo(nodes[i].children[0].data);
+                        },randTime);
                     }
-
-                    return res.json(
-                    {
-                        code: 200,
-                    });
-
-                },function(err)
-                {
-                    return res.json(
-                    {
-                        code: 500,
-                        data: err,
-                    });
-                },cookie);
+                });
             },randTime);
-        });
+        }
     });
 });
 
-function getPhoneName(phoneName,func,errFunc)
-{
-    let sql="select * from phoneprice_phonename where models like '%"+phoneName+"%';";
-    conn.query(sql,function(err,ret)
-    {
-        if(err)
-        {
-            console.log(err);
-            if(errFunc)
-            {
-                errFunc(err);
-                return;
-            }
-        }
-
-        lists=new Array();
-        ret.forEach(element => {
-            lists.push(element);
-        });
-
-        if(func)
-        {
-            func(lists);
-        }
-    });
-}
-
 function getPhoneNames(func,errFunc)
 {
-    let sql="select * from phoneprice_phonename;";
+    let sql="select name from phoneprice_phoneinfo;";
     conn.query(sql,function(err,ret)
     {
         if(err)
@@ -466,4 +354,119 @@ function updateSql(tableName,id,keys,values,func,errFunc)
             func(ret);
         }
     });
+}
+
+function searchInfo(phoneName)
+{
+    let url="http://detail.zol.com.cn/index.php?c=SearchList&subcateId=57&keyword="+phoneName;
+    url=encodeURI(url);
+    let cookie="Hm_lpvt_ae5edc2bc4fc71370807f6187f0a2dd0="+Date.parse(new Date());
+    httpRequest(url,function(content)
+    {
+        content=iconv.decode(content,"gb2312");
+        let $=cheerio.load(content);
+        let mods=$("div[class='list-item clearfix']");
+        let prices=$("div[class='price-box']");
+
+        for(let i=0;i<mods.length;i++)
+        {
+            let it=mods[i];
+            let priceNode=prices[i];
+
+            let phone=new PhoneInfo();
+
+            phone.name=$("h3 a",it).text();
+            phone.price=$("b[class='price-type']",priceNode).text();
+            phone.link=$("ul[class='param clearfix'] li:nth-child(8) a",it).attr("href");
+            if(phone.link=="")
+            {
+                continue;
+            }
+
+            phone.link="http://detail.zol.com.cn/"+phone.link;
+
+            let randTime=Math.random()*mods.length*10000+3000;
+            setTimeout(function()
+            {
+                httpRequest(phone.link,function(content)
+                {
+                    content=iconv.decode(content,"gb2312");
+                    let $=cheerio.load(content);
+                    let detailedParameters=$("div[class='detailed-parameters']");
+
+                    let ths=$("th",detailedParameters);
+                    let tds=$("td",detailedParameters);
+                    for(let i=0;i<ths.length;i++)
+                    {
+                        let title=$("#newPmName_"+i,ths).text();
+                        let value=$("#newPmVal_"+i,tds).text();
+
+                        switch(title)
+                        {
+                        case "出厂系统内核":
+                            phone.system=value;
+                            break;
+                        case "CPU型号":
+                            phone.CPU=value;
+                            break;
+                        case "主屏分辨率":
+                            phone.mainScreen=value;
+                            break;
+                        case "CPU频率":
+                            phone.CPUrate=value;
+                            break;
+                        case "GPU型号":
+                            phone.GPU=value;
+                            break;
+                        case "RAM容量":
+                            phone.RAM=value;
+                            break;
+                        case "ROM容量":
+                            phone.ROM=value;
+                            break;
+                        case "电池容量":
+                            phone.power=value;
+                            break;
+                        case "上市日期":
+                            phone.markTime=value;
+                            break;
+                        case "屏幕类型":
+                            phone.mainScreenType=value;
+                            break;
+                        }
+                    }
+
+                    let sqlKeys=
+                    [
+                        "name","mainScreen","CPU",
+                        "CPUrate","system","power",
+                        "price","link","GPU",
+                        "RAM","ROM","markTime",
+                        "mainScreenType","updateTime"
+                    ];
+
+                    let sqlValue=
+                    [
+                        phone.name,phone.mainScreen,phone.CPU,
+                        phone.CPUrate,phone.system,phone.power,
+                        phone.price,phone.link,phone.GPU,
+                        phone.RAM,phone.ROM,phone.markTime,
+                        phone.mainScreenType,"now()"
+                    ];
+
+                    getPhoneInfoID(phone.name,function(id)
+                    {
+                        if(id==0)
+                        {
+                            insertSql("phoneprice_phoneinfo",sqlKeys,sqlValue);
+                        }
+                        else
+                        {
+                            updateSql("phoneprice_phoneinfo",id,sqlKeys,sqlValue);
+                        }
+                    });
+                });
+            },randTime);
+        }
+    },null,cookie);
 }
